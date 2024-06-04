90d9ee0ebe8901e93ad454d02a958b5c1b4be662
anderDaZu <anderda-96@hotmail.com>
2024-06-03 22:32:58 -0500

    Mostrar detalles de un registro de una api

--- src/views/courses/CourseDetailsView.vue
+++ src/views/courses/CourseDetailsView.vue
@@ -1,19 +1,40 @@
 <template>
-    <h2>Detalle de Curso</h2>
+    <h2>{{ course.title }}</h2>
+
+    <p>{{ course.description }}</p>
+
+    <p>
+        <b>Categoría:</b>
+        {{ category.name }}
+    </p>
+    <!-- {{ course }} -->
 </template>
 
 <script>
 export default {
     data(){
         return {
-
+            course: {},
+            category: {}
         }
     },
     props: [
 
     ],
+    created(){
+        this.getCourse()
+    },
     methods: {
-
+        getCourse(){
+            this.axios.get('http://academy.test/api/courses/' + this.$route.params.id + '?included=category')
+                .then(response => {
+                    this.course = response.data
+                    this.category = response.data.category
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
     }
 }
 </script>
